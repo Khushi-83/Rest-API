e07318bfc79e70e738d915f5572727cf4d2707d4
Khushi Sinha <ks1393990@gmail.com>
2024-08-05 18:56:16 +0530

    chore: Update REST API endpoints and server configuration

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require ("express");
 const app = express();
 const users = require ("./MOCK_DATA.json");
-const port = 8000;
+const PORT = 8000;
 
+app.use(express.urlencoded({extented: false}));
 app.get("/users", (req, res) =>{
-    return res.json(users)
-})
+    const html = `
+    <ul>
+       ${users.map((user) => `<li>${user.first_name}</li>`).join('')}
+    </ul>
+    `;
+       res.send(html);
+});
 
-app.listen(port, () => console.log(`Server started at port ($PORT)`))
+app.get("/api/users", (req, res) =>{
+    return res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id); 
+    return res.json(user);
+});
+
+app.post("/api/users", (req, res) =>{
+    const body = req.body;
+    console.log("Body", body);
+    return res.json(users);
+});
+  
+app.listen(PORT, () => console.log(`Server started at port ($PORT)`));
